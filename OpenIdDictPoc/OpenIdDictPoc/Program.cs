0d48dc4556f184a800c048c9d6f6d72dac3496a4
Alessandro Miranda <alessandromiranda@gipo.it>
2023-07-13 11:29:05 +0200

    add nuget

--- OpenIdDictPoc/OpenIdDictPoc/Program.cs
+++ OpenIdDictPoc/OpenIdDictPoc/Program.cs
@@ -1,9 +1,40 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using OpenIddict.Server.AspNetCore;
+using static OpenIddict.Abstractions.OpenIddictConstants;
+using System.Security.Claims;
+using OpenIddict.Abstractions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenIddict()
+    .AddServer(options =>
+    {
+        // Enable the authorization and token endpoints.
+        options.SetAuthorizationEndpointUris("/authorize")
+            .SetTokenEndpointUris("/token");
+
+        // Enable the authorization code flow.
+        options.AllowAuthorizationCodeFlow();
+
+        // Register the signing and encryption credentials.
+        options.AddDevelopmentEncryptionCertificate()
+            .AddDevelopmentSigningCertificate();
+
+        // Register the ASP.NET Core host and configure the authorization endpoint
+        // to allow the /authorize minimal API handler to handle authorization requests
+        // after being validated by the built-in OpenIddict server event handlers.
+        //
+        // Token requests will be handled by OpenIddict itself by reusing the identity
+        // created by the /authorize handler and stored in the authorization codes.
+        options.UseAspNetCore()
+            .EnableAuthorizationEndpointPassthrough();
+    });
 
 var app = builder.Build();
 
@@ -35,6 +66,8 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+
 
 app.Run();
 
